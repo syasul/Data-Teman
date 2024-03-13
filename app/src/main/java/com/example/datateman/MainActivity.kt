2e24575c86ece0c731d50fbde7913e1ea5e3d1c2
syasul <githubku4@gmail.com>
2024-03-13 11:45:48 +0700

    logout

--- app/src/main/java/com/example/datateman/MainActivity.kt
+++ app/src/main/java/com/example/datateman/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.datateman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.datateman.databinding.ActivityLoginBinding
+import com.example.datateman.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var auth: FirebaseAuth? = null
+    private val RC_SIGN_IN = 1
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener(this)
+        binding.showData.setOnClickListener(this)
+        binding.save.setOnClickListener(this)
+
+        auth = FirebaseAuth.getInstance()
     }
+
+    private fun isEmpty(s: String): Boolean {
+        return TextUtils.isEmpty(s)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.getId()) {
+            R.id.save -> {}
+            R.id.logout -> {
+                AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(object : OnCompleteListener<Void> {
+                        override fun onComplete(p0: Task<Void>) {
+                            Toast.makeText(this@MainActivity, "Logout Berhasil",
+                                Toast.LENGTH_SHORT).show()
+                            intent = Intent(applicationContext, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    })
+            }
+            R.id.showData -> {}
+        }
+    }
+
 }
